801f78b2777683b8b8a87d0e2b747baae3bd82da
FIRST_NAME LAST_NAME <offical10malik@gmail.com>
2026-01-03 10:41:24 +0530

    Fix images using Vite imports (production-safe)

--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -1,69 +1,38 @@
 // Restaurant images
-import r1 from "../assets/images/restaurants/r1.jpg";
-import r2 from "../assets/images/restaurants/r2.png";
-import r3 from "../assets/images/restaurants/r3.jpg";
-import r4 from "../assets/images/restaurants/r4.jpg";
-import r5 from "../assets/images/restaurants/r5.jpg";
-
 export const restaurantImages = {
-  r1,
-  r2,
-  r3,
-  r4,
-  r5,
+  r1: "/src/assets/images/restaurants/r1.jpg",
+  r2: "/src/assets/images/restaurants/r2.png",
+  r3: "/src/assets/images/restaurants/r3.jpg",
+  r4: "/src/assets/images/restaurants/r4.jpg",
+  r5: "/src/assets/images/restaurants/r5.jpg"
 };
 
 // Menu images
-import m1 from "../assets/images/menu/m1.jpg";
-import m2 from "../assets/images/menu/m2.jpg";
-import m3 from "../assets/images/menu/m3.jpg";
-import m4 from "../assets/images/menu/m4.jpg";
-import m5 from "../assets/images/menu/m5.jpg";
-import m6 from "../assets/images/menu/m6.jpg";
-import m7 from "../assets/images/menu/m7.jpg";
-import m8 from "../assets/images/menu/m8.jpg";
-import m9 from "../assets/images/menu/m9.jpg";
-import m10 from "../assets/images/menu/m10.jpg";
-import m11 from "../assets/images/menu/m11.jpg";
-import m12 from "../assets/images/menu/m12.jpg";
-import m13 from "../assets/images/menu/m13.jpg";
-import m14 from "../assets/images/menu/m14.jpg";
-import m15 from "../assets/images/menu/m15.jpg";
-import m16 from "../assets/images/menu/m16.jpg";
-import m17 from "../assets/images/menu/m17.jpg";
-import m18 from "../assets/images/menu/m18.jpg";
-import m19 from "../assets/images/menu/m19.jpg";
-import m20 from "../assets/images/menu/m20.jpg";
-import m21 from "../assets/images/menu/m21.jpg";
-import m22 from "../assets/images/menu/m22.jpg";
-import m23 from "../assets/images/menu/m23.jpg";
-import m24 from "../assets/images/menu/m24.jpg";
-import m25 from "../assets/images/menu/m25.jpg";
-
 export const menuImages = {
-  m1,
-  m2,
-  m3,
-  m4,
-  m5,
-  m6,
-  m7,
-  m8,
-  m9,
-  m10,
-  m11,
-  m12,
-  m13,
-  m14,
-  m15,
-  m16,
-  m17,
-  m18,
-  m19,
-  m20,
-  m21,
-  m22,
-  m23,
-  m24,
-  m25,
+  m1: "/src/assets/images/menu/m1.jpg",
+  m2: "/src/assets/images/menu/m2.jpg",
+  m3: "/src/assets/images/menu/m3.jpg",
+  m4: "/src/assets/images/menu/m4.jpg",
+  m5: "/src/assets/images/menu/m5.jpg",
+  m6: "/src/assets/images/menu/m6.jpg",
+  m7: "/src/assets/images/menu/m7.jpg",
+  m8: "/src/assets/images/menu/m8.jpg",
+  m9: "/src/assets/images/menu/m9.jpg",
+  m10: "/src/assets/images/menu/m10.jpg", 
+  m11: "/src/assets/images/menu/m11.jpg",
+  m12: "/src/assets/images/menu/m12.jpg",
+  m13: "/src/assets/images/menu/m13.jpg",
+  m14: "/src/assets/images/menu/m14.jpg",   
+  m15: "/src/assets/images/menu/m15.jpg",
+  m16: "/src/assets/images/menu/m16.jpg",
+  m17: "/src/assets/images/menu/m17.jpg",
+  m18: "/src/assets/images/menu/m18.jpg",
+  m19: "/src/assets/images/menu/m19.jpg",
+  m20: "/src/assets/images/menu/m20.jpg",
+  m21: "/src/assets/images/menu/m21.jpg",
+  m22: "/src/assets/images/menu/m22.jpg",
+  m23: "/src/assets/images/menu/m23.jpg",
+  m24: "/src/assets/images/menu/m24.jpg",
+  m25: "/src/assets/images/menu/m25.jpg"
+
 };
